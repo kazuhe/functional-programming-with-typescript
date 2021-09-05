0bc962bad47cafe47d71d549e31c2dd6f192a2f5
kazuhe <omc.ohara@gmail.com>
2021-09-06 00:25:07 +0900

    Convert from array of odd values to array of even values

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 import { succ } from './succ'
+import { mapper } from './mapper'
 
-const num = 1
-console.log(succ(num))
+/**
+ * 関数を組み合わせて奇数の値を持った配列から偶数の値を持つ配列に変換する
+ */
+const toEven = mapper(succ)
+const oddArray = [1, 3, 5]
+console.log(toEven(oddArray))
