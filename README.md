312c45fb4771a79d0248c50d6d2fb809797f9fd4
kazuhe <omc.ohara@gmail.com>
2022-01-25 23:28:43 +0900

    added ramda sample

--- README.md
+++ README.md
@@ -13,6 +13,24 @@ tr 'A-Z' 'a-z' < text | uniq | sort
 状態の不在と不変性を可能な限り追求し、プログラムの理解を容易にするパラダイムである。
 
 ## memo
+- [ ] データ定義文（DDL=Data Definition Language)
+  - [ ] CREATE: データベースオブジェクト(テーブル、インデックス、制約など)を定義する
+  - [ ] DROP: データベースオブジェクトを削除する
+  - [ ] ALTER: データベースオブジェクトの定義を変更する
+- [ ] データ操作文（DML=Data Manipulate Language)
+  - [ ] INSERT INTO: レコードを挿入
+  - [ ] UPDATE ～ SET: レコードを更新
+  - [ ] DELETE FROM: レコードを削除
+  - [ ] SELECT ～ FROM ～ WHERE: レコードを検索し、結果集合を取り出す
+
+データが「どのように」得られるのかではなく、データの出力が「何か」をコードで説明する
+
+SQLライクな javascript にする？構成
+
+```
+_.from(persons).where(p=>p.birthYear>1900&&p.address.country!=='US').sortBy(['firstname']).select(p=>p.firstname).value();
+```
+
 - [ ] 作成
   - [ ] オブジェクト
   - [ ] 配列
--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,66 @@ console.log(toEven(oddArray));
 
 // 基本的な逐次パイプライン
 console.log(normalize(trim("777-7777-7777 ")));
+
+/**
+ * ramda サンプル
+ */
+import { prop, assoc, lens, view, set, lensPath, lensProp } from "ramda";
+
+type User = {
+  name: string;
+  email: string;
+  age: number;
+  foo?: string;
+  address: {
+    zipcode: string;
+  };
+};
+
+const user: User = {
+  name: "Bret",
+  email: "bred@april.biz",
+  age: 22,
+  address: {
+    zipcode: "92998-3874",
+  },
+};
+Object.freeze(user);
+
+// プロパティにアクセス
+console.log("prop:", prop("name", user)); // => prop: Bret
+
+// プロパティを変更する（副作用なしで新しいオブジェクトを返す）
+const foo = assoc<number, User, string>("age", 23, user);
+console.log("foo:", foo, "\nuser:", user);
+// =>
+// foo: {
+//   name: 'Bret',
+//   email: 'bred@april.biz',
+//   age: 23,
+//   address: { zipcode: '92998-3874' }
+// }
+// user: {
+//   name: 'Bret',
+//   email: 'bred@april.biz',
+//   age: 22,
+//   address: { zipcode: '92998-3874' }
+// }
+
+// lens => getter や setter を関数型プログラミング的に抽象化したもの
+const ageLens = lens(prop<string>("age"), assoc<string>("age"));
+const ageLens2 = lensProp<User>("age"); // 上のシンタックスシュガー的なやつ
+console.log("view:", view(ageLens, user)); // => view: 22
+console.log("view:", view(ageLens2, user)); // => view: 22
+console.log("set:", set(ageLens, 28, user));
+// =>
+// set: {
+//   name: 'Bret',
+//   email: 'bred@april.biz',
+//   age: 28,
+//   address: { zipcode: '92998-3874' }
+// }
+
+// ネストしたプロパティに直接アクセス
+const zipcodeLens = lensPath(["address", "zipcode"]);
+console.log("view:", view(zipcodeLens, user)); // => view: 92998-3874
